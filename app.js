3e495a57871f7319e83d08c9909f310f7b9fc415
daudi13 <davouma13@gmail.com>
2021-06-17 04:40:35 +0300

    commit js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const btn = document.getElementById('btn');
 
+
+btn.addEventListener('click', () => {
+    const bill = Number(document.getElementById('bill').value);
+    const tip = Number(document.getElementById('tip').value)
+    
+    calcTip(bill, tip);
+})
+
+function calcTip(bill, tip) {
+    console.log(bill * tip/100)
+}
