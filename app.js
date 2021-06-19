fdfbeba4b333587109b00ffc36bdd1417b7c9aeb
daudi13 <davouma13@gmail.com>
2021-06-19 05:41:41 +0300

    commit js updates

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
 
 
 
 btn.addEventListener('click', () => {
     const bill = Number(document.getElementById('bill').value);
-    const tip = Number(document.getElementById('tip').value)
+    const tip = Number(document.getElementById('tip').value);
+    calcTip(bill, tip);
 })
 
 function calcTip(bill, tip) {
-    const NewTotal = document.getElementById('result').textContent = `[${(bill * tip / 100)}]`
-    return NewTotal;
+    const newTotal = document.getElementById('result').textContent = `[${(bill * tip / 100)}]`;
+
+    console.log(result.textContent = newTotal)
 }
+
+
+
+// btn.addEventListener('click', calcTip())
+
+// function calcTip() {
+//     const bill = Number(document.getElementById('bill').value);
+//     const tip = Number(document.getElementById('tip').value);
+
+//     const newTotal = result.textContent = `[${(bill * tip / 100)}]`;
+
+//     return newTotal
+// }
